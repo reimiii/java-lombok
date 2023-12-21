ce05b4e75c38e9b802a156de1af37a9cc5aacc6a
Hilmi AM <imiia75775@proton.me>
2023-12-21 20:38:44 +0700

    Initial commit

---
+++ src/main/java/franxx/code/lombok/JavaLombokApplication.java
@@ -0,0 +1,13 @@
+package franxx.code.lombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLombokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/franxx/code/lombok/JavaLombokApplicationTests.java
@@ -0,0 +1,13 @@
+package franxx.code.lombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLombokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
