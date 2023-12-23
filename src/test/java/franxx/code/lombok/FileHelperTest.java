18682db117265ac7687dead3633d8849aa9cfe51
Hilmi AM <imiia75775@proton.me>
2023-12-23 11:36:29 +0700

    SneakyThrows

--- src/test/java/franxx/code/lombok/FileHelperTest.java
+++ src/test/java/franxx/code/lombok/FileHelperTest.java
@@ -1,11 +1,21 @@
 package franxx.code.lombok;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
 
 public class FileHelperTest {
 
     @Test
     void file() throws Exception {
         System.out.println(FileHelper.loadFile("pom.xml"));
+    }
+
+    @Test
+    void fileNotFound() {
+        Assertions.assertThrows(FileNotFoundException.class, () -> {
+           FileHelper.loadFile("pommm.xml");
+        });
     }
 }
