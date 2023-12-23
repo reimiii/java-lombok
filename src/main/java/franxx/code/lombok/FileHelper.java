072f23f8268f8c5f301e60ad25236bb025ad5ca8
Hilmi AM <imiia75775@proton.me>
2023-12-23 11:48:51 +0700

    Logging in Lombok using @Slf4j

--- src/main/java/franxx/code/lombok/FileHelper.java
+++ src/main/java/franxx/code/lombok/FileHelper.java
@@ -2,14 +2,19 @@ package franxx.code.lombok;
 
 import lombok.Cleanup;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.FileReader;
 import java.util.Scanner;
 
+@Slf4j
 public class FileHelper {
 
     @SneakyThrows
     public static String loadFile(String file) {
+
+        log.info("load file {}", file);
+
         @Cleanup FileReader fileReader = new FileReader(file);
         @Cleanup Scanner scanner = new Scanner(fileReader);
 
